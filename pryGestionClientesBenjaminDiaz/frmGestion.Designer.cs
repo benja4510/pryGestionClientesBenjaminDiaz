c0621d97cf29f727fb9aa98e989ac6787501bc60
Alumno <Alumno@PC29>
2026-04-06 12:01:04 -0300

    Agregar archivos de proyecto.

---
+++ pryGestionClientesBenjaminDiaz/frmGestion.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace pryGestionClientesBenjaminDiaz
+{
+    partial class frmGestion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.gboCarga = new System.Windows.Forms.GroupBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblDeuda = new System.Windows.Forms.Label();
+            this.txtDeuda = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblLimite = new System.Windows.Forms.Label();
+            this.txtLimite = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.gboCarga.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(22, 42);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Codigo:";
+            // 
+            // gboCarga
+            // 
+            this.gboCarga.Controls.Add(this.btnCargar);
+            this.gboCarga.Controls.Add(this.txtLimite);
+            this.gboCarga.Controls.Add(this.lblLimite);
+            this.gboCarga.Controls.Add(this.txtUsuario);
+            this.gboCarga.Controls.Add(this.lblUsuario);
+            this.gboCarga.Controls.Add(this.txtDeuda);
+            this.gboCarga.Controls.Add(this.lblDeuda);
+            this.gboCarga.Controls.Add(this.txtCodigo);
+            this.gboCarga.Controls.Add(this.lblCodigo);
+            this.gboCarga.Location = new System.Drawing.Point(29, 26);
+            this.gboCarga.Name = "gboCarga";
+            this.gboCarga.Size = new System.Drawing.Size(463, 161);
+            this.gboCarga.TabIndex = 1;
+            this.gboCarga.TabStop = false;
+            this.gboCarga.Text = "Carga de Datos";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(71, 39);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // lblDeuda
+            // 
+            this.lblDeuda.AutoSize = true;
+            this.lblDeuda.Location = new System.Drawing.Point(22, 83);
+            this.lblDeuda.Name = "lblDeuda";
+            this.lblDeuda.Size = new System.Drawing.Size(42, 13);
+            this.lblDeuda.TabIndex = 2;
+            this.lblDeuda.Text = "Deuda:";
+            // 
+            // txtDeuda
+            // 
+            this.txtDeuda.Location = new System.Drawing.Point(71, 83);
+            this.txtDeuda.Name = "txtDeuda";
+            this.txtDeuda.Size = new System.Drawing.Size(100, 20);
+            this.txtDeuda.TabIndex = 3;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(247, 42);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 4;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(315, 39);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // lblLimite
+            // 
+            this.lblLimite.AutoSize = true;
+            this.lblLimite.Location = new System.Drawing.Point(228, 90);
+            this.lblLimite.Name = "lblLimite";
+            this.lblLimite.Size = new System.Drawing.Size(92, 13);
+            this.lblLimite.TabIndex = 6;
+            this.lblLimite.Text = "Limite de Clientes:";
+            // 
+            // txtLimite
+            // 
+            this.txtLimite.Location = new System.Drawing.Point(315, 87);
+            this.txtLimite.Name = "txtLimite";
+            this.txtLimite.Size = new System.Drawing.Size(100, 20);
+            this.txtLimite.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvClientes);
+            this.groupBox1.Location = new System.Drawing.Point(28, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(473, 245);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Consulta de Datos:";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(315, 132);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvClientes.Location = new System.Drawing.Point(6, 31);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.Size = new System.Drawing.Size(458, 208);
+            this.dgvClientes.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Codigo";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Usuario";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Limite de Credito";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Deuda";
+            this.Column4.Name = "Column4";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(341, 486);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(84, 13);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total de Deuda:";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(426, 514);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 10;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(431, 483);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 11;
+            // 
+            // frmGestion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 549);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gboCarga);
+            this.Name = "frmGestion";
+            this.Text = "Gestion de Clientes";
+            this.Load += new System.EventHandler(this.frmGestion_Load);
+            this.gboCarga.ResumeLayout(false);
+            this.gboCarga.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.GroupBox gboCarga;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtDeuda;
+        private System.Windows.Forms.Label lblDeuda;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.TextBox txtLimite;
+        private System.Windows.Forms.Label lblLimite;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ pryGestionClientesBenjaminDiaz/frmGestion.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryGestionClientesBenjaminDiaz
+{
+
+    public partial class frmGestion : Form
+    {
+        public frmGestion()
+        {
+            InitializeComponent();
+        }
+
+        //Registro
+        private struct RegCliente
+        {
+            public Int32 Codigo;
+            public String Usuario;
+            public Decimal Deuda;
+            public Decimal Limite;
+        }
+
+        //vector 
+        private RegCliente[] Clientes = new RegCliente[10];
+
+        //Indice
+        private Int32 IND = 0;
+
+
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            //if para validar que el indice no supere la cantidad de clientes que se pueden cargar, sino muestra un mensaje de error
+            if (IND < Clientes.Length)
+            {
+                Clientes[IND].Codigo = Convert.ToInt32(txtCodigo.Text);
+                Clientes[IND].Usuario = txtUsuario.Text;
+                Clientes[IND].Deuda = Convert.ToDecimal(txtDeuda.Text);
+                Clientes[IND].Limite = Convert.ToDecimal(txtLimite.Text);
+                IND++; //O IND+1;
+                MessageBox.Show("Cliente cargado correctamente");
+                //Limpio los txt para cargar otro cliente
+                txtCodigo.Text = "";
+                txtDeuda.Text = "";
+                txtLimite.Text = "";
+                txtUsuario.Text = "";
+
+            }
+            else
+            {
+                MessageBox.Show("No se pueden cargar mas clientes");
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            Decimal Total = 0; //Variable para acumular la deuda total de los clientes
+            dgvClientes.Rows.Clear(); //Limpio la grilla para mostrar los clientes cargados
+            for (Int32 i = 0; i < IND; i++)
+            {
+                //Agrego a la grilla los clientes cargados, mostrando su codigo, usuario, deuda y limite
+                dgvClientes.Rows.Add("Codigo: " + Clientes[i].Codigo + " Usuario: " + Clientes[i].Usuario + " Limite " + Clientes[i].Limite + " Deuda: " + Clientes[i].Deuda);
+                Total += Total + Clientes[i].Deuda; //Acumulo la deuda total de los clientes
+
+
+            }
+            lblTotal.Text = Total.ToString(); //Muestro la deuda total de los clientes en el label lblTotal
+            lblTotal.Visible = true;  //Hago visible el label lblTotal para mostrar la deuda total de los clientes
+        }
+
+      
+
+
+        private void frmGestion_Load(object sender, EventArgs e)
+        {
+            btnCargar.Enabled = false; //Deshabilito el boton cargar al iniciar el formulario, para evitar que se carguen clientes sin completar los campos
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            if(txtCodigo.Text != "" && txtUsuario.Text != "" && txtDeuda.Text != "" && txtLimite.Text != "") //Si los campos no estan vacios, habilito el boton cargar
+            {
+                btnCargar.Enabled = true;
+            }
+            else
+            {
+                btnCargar.Enabled = false; //Si alguno de los campos esta vacio, deshabilito el boton cargar
+            }
+        }
+    }
+
+}
